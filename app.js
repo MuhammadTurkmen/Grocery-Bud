781b5395ef717f1b02d7f9dd586751aa1d4d4707
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-20 07:29:01 +0430

    init

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ function setBackToDefault() {
 // ****** LOCAL STORAGE **********
 function addToLocalStorage(id, value) {
     const grocery = {id, value}
-    let items = localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : []
+    let items = 
     items.push(grocery)
     localStorage.setItem('list', JSON.stringify(items))
 }
@@ -142,7 +142,7 @@ function editLocalStorage(id, value) {
 }
 
 function getLocalStorage() {
-    
+    return localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : []
 }
 // ****** SETUP ITEMS **********
  
